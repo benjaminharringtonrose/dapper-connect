869b0c401430ea198931ade5df4ceb75ab498437
benjamin <benjaminharringtonrose@gmail.com>
2021-12-29 16:13:20 -0500

    feat: cleaner postinstall hack

--- scripts/postinstall.ts
+++ scripts/postinstall.ts
@@ -1,6 +1,8 @@
 import "dotenv/config";
 import * as child_process from "child_process";
 
+import * as fs from "fs-extra";
+
 // Uncommment to rename the application binaries on postinstall.
 //const {APP_DISPLAY_NAME} = process.env;
 //child_process.execSync(
@@ -14,15 +16,33 @@ import * as child_process from "child_process";
 //  {stdio: 'inherit'}
 //);
 
+const PROJECT_ROOT = ".";
+
+function removeBadNodeModules() {
+  const gestureHandlerDir = `${PROJECT_ROOT}/node_modules/@rainbow-me/animated-charts/node_modules/react-native-gesture-handler`;
+  const gestureHandlerDirExists = fs.existsSync(gestureHandlerDir);
+
+  if (gestureHandlerDirExists) {
+    fs.removeSync(gestureHandlerDir);
+    console.log(`Removed ${gestureHandlerDir}`);
+  } else {
+    console.log(`${gestureHandlerDir} not found. Ignoring.`);
+  }
+
+  const reactNativeSvgDir = `${PROJECT_ROOT}/node_modules/@walletconnect/react-native-dapp/node_modules/react-native-svg`;
+  const reactNativeSvgDirExists = fs.existsSync(reactNativeSvgDir);
+
+  if (reactNativeSvgDirExists) {
+    fs.removeSync(reactNativeSvgDir);
+    console.log(`Removed ${reactNativeSvgDir}`);
+  } else {
+    console.log(`${reactNativeSvgDir} not found. Ignoring.`);
+  }
+}
+
+removeBadNodeModules();
+
 child_process.execSync("npx patch-package", { stdio: "inherit" });
-
-child_process.execSync(
-  "rm -rf node_modules/@rainbow-me/animated-charts/node_modules/react-native-gesture-handler"
-);
-
-child_process.execSync(
-  "rm -rf node_modules/@walletconnect/react-native-dapp/node_modules/react-native-svg"
-);
 
 // Uncomment to reinstall pods on postinstall.
 // import {macos} from 'platform-detect';
